fa966cea37b7ccbc7ef3071fac6da3bbcdaf3e52
Frank Alvino <71982017+frankalvino@users.noreply.github.com>
2022-01-29 12:21:06 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+#comments
+# input phase
+qty = float(input("Enter quantity ordered"))
+ppu = float(input("Enter price per unit"))
+
+#procee phase
+extp = qty * ppu
+
+#output
+print("Extended Price is $", extp)
+
